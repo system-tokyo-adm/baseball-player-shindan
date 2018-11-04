139fa392cdf8d33b19c0f877fb3886a72dc978da
Keita Yamada <system_tokyo_adm@yahoo.co.jp>
2018-11-04 19:51:26 +0900

    ui上のタグ名を修正

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -17,84 +17,84 @@
 
                         <!-- Title Name -->
                         <div class="form-group">
-                            <label for="title-name" class="col-sm-3 control-label">打率</label>
+                            <label for="ave-name" class="col-sm-3 control-label">打率</label>
 
                             <div class="col-sm-6">
-                                <input type="text" name="title" id="title-name" class="form-control" value="{{ old('title') }}">
+                                <input type="number" name="ave" id="ave-name" class="form-control" value="{{ old('ave') }}">
                             </div>
                         </div>
 
-                        <!-- Title Description -->
+                        <!-- TPA -->
                         <div class="form-group">
-                            <label for="title-name" class="col-sm-3 control-label">打席</label>
+                            <label for="tpa-name" class="col-sm-3 control-label">打席</label>
 
                             <div class="col-sm-6">
-                                <input type="text" name="detail" id="detail-name" class="form-control" value="{{ old('title') }}">
+                                <input type="number" name="tpa" id="tpa-name" class="form-control" value="{{ old('tpa') }}">
                             </div>
                         </div>
 
-                        <!-- Title Description -->
+                        <!-- Bats -->
                         <div class="form-group">
-                            <label for="title-name" class="col-sm-3 control-label">打数</label>
+                            <label for="bats-name" class="col-sm-3 control-label">打数</label>
 
                             <div class="col-sm-6">
-                                <input type="text" name="detail" id="detail-name" class="form-control" value="{{ old('title') }}">
+                                <input type="number" name="bats" id="bats-name" class="form-control" value="{{ old('bats') }}">
                             </div>
                         </div>
 
-                        <!-- Title Description -->
+                        <!-- Hits  Description -->
                         <div class="form-group">
-                            <label for="title-name" class="col-sm-3 control-label">安打</label>
+                            <label for="hits-name" class="col-sm-3 control-label">安打</label>
 
                             <div class="col-sm-6">
-                                <input type="text" name="detail" id="detail-name" class="form-control" value="{{ old('title') }}">
-                            </div>
-                        </div>
-
-
-                        <!-- Title Description -->
-                        <div class="form-group">
-                            <label for="title-name" class="col-sm-3 control-label">本塁打</label>
-
-                            <div class="col-sm-6">
-                                <input type="text" name="detail" id="detail-name" class="form-control" value="{{ old('title') }}">
+                                <input type="number" name="hits" id="hits-name" class="form-control" value="{{ old('hits') }}">
                             </div>
                         </div>
 
 
-                        <!-- Title Description -->
+                        <!-- HR -->
                         <div class="form-group">
-                            <label for="title-name" class="col-sm-3 control-label">打点</label>
+                            <label for="hr-name" class="col-sm-3 control-label">本塁打</label>
 
                             <div class="col-sm-6">
-                                <input type="text" name="detail" id="detail-name" class="form-control" value="{{ old('title') }}">
+                                <input type="number" name="hr" id="hr-name" class="form-control" value="{{ old('hr') }}">
                             </div>
                         </div>
 
-                        <!-- Title Description -->
+
+                        <!-- RBI  -->
                         <div class="form-group">
-                            <label for="title-name" class="col-sm-3 control-label">得点</label>
+                            <label for="rbi-name" class="col-sm-3 control-label">打点</label>
 
                             <div class="col-sm-6">
-                                <input type="text" name="detail" id="detail-name" class="form-control" value="{{ old('title') }}">
+                                <input type="number" name="rbi" id="rbi-name" class="form-control" value="{{ old('rbi') }}">
                             </div>
                         </div>
 
-                        <!-- Title Description -->
+                        <!-- score -->
                         <div class="form-group">
-                            <label for="title-name" class="col-sm-3 control-label">盗塁</label>
+                            <label for="score-name" class="col-sm-3 control-label">得点</label>
 
                             <div class="col-sm-6">
-                                <input type="text" name="detail" id="detail-name" class="form-control" value="{{ old('title') }}">
+                                <input type="number" name="score" id="score-name" class="form-control" value="{{ old('score') }}">
                             </div>
                         </div>
 
-                        <!-- Title Description -->
+                        <!-- steal -->
                         <div class="form-group">
-                            <label for="title-name" class="col-sm-3 control-label">出塁率</label>
+                            <label for="steal-name" class="col-sm-3 control-label">盗塁</label>
 
                             <div class="col-sm-6">
-                                <input type="text" name="detail" id="detail-name" class="form-control" value="{{ old('title') }}">
+                                <input type="number" name="steal" id="steal-name" class="form-control" value="{{ old('steal') }}">
+                            </div>
+                        </div>
+
+                        <!-- basep -->
+                        <div class="form-group">
+                            <label for="basep-name" class="col-sm-3 control-label">出塁率</label>
+
+                            <div class="col-sm-6">
+                                <input type="number" name="basep" id="basep-name" class="form-control" value="{{ old('basep') }}">
                             </div>
                         </div>
 
