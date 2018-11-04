7819f9e7eaab6d6a387fbe89e6182274555dbbfa
Keita Yamada <system_tokyo_adm@yahoo.co.jp>
2018-11-04 17:00:31 +0900

    入力項目の配置の変更

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -2,7 +2,7 @@
 
 @section('content')
     <div class="container">
-        <div class="col-sm-offset-2 col-sm-8">
+        <div class="col-sm-offset-8 col-sm-4">
             <div class="panel panel-default">
                 <div class="panel-heading">
                     成績を入力する 
