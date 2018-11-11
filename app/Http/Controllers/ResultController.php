30200252f32730013961f3940ee42dab573fc028
Keita Yamada <system_tokyo_adm@yahoo.co.jp>
2018-11-11 15:18:27 +0900

    入力値が存在しない場合はスキップ

--- app/Http/Controllers/ResultController.php
+++ app/Http/Controllers/ResultController.php
@@ -76,49 +76,67 @@ class ResultController extends Controller
         $num_deci = array(0,0,0,0,0,0,0,0,0);
 
         //打率
-        $m_aves   = array(0.301,0.272,0.329,0.287,0.254,0.291,0.271,0.300,0.215);
-        $m_ave_index = $this->fix_top_number($m_aves,$ave);
-        $num_deci[$m_ave_index] += 1;
+        if(!is_null($ave)){
+            $m_aves   = array(0.301,0.272,0.329,0.287,0.254,0.291,0.271,0.300,0.215);
+            $m_ave_index = $this->fix_top_number($m_aves,$ave);
+            $num_deci[$m_ave_index] += 1;
+        }
 
         //打席
-        $m_tpas       = array(3627,4076,4998,3443,4883,6615,6059,4648,1539);
-        $m_tpa_index = $this->fix_top_number($m_tpas,$tpa);
-        $num_deci[$m_tpa_index] += 1;
+        if(!is_null($tpa)){
+            $m_tpas       = array(3627,4076,4998,3443,4883,6615,6059,4648,1539);
+            $m_tpa_index = $this->fix_top_number($m_tpas,$tpa);
+            $num_deci[$m_tpa_index] += 1;
+        }
 
         //打数
-        $m_bats   = array(3078,3576,4395,2962,4378,5885,5522,4084,1328);
-        $m_bat_index = $this->fix_top_number($m_bats,$bats);
-        $num_deci[$m_bat_index] += 1;
+        if(!is_null($bats)){
+            $m_bats   = array(3078,3576,4395,2962,4378,5885,5522,4084,1328);
+            $m_bat_index = $this->fix_top_number($m_bats,$bats);
+            $num_deci[$m_bat_index] += 1;
+        }
 
         //安打
-        $m_hits   = array(927,974,1446,851,1111,1711,1499,1226,285);
-        $m_hit_index = $this->fix_top_number($m_hits,$hits);
-        $num_deci[$m_hit_index] += 1;
+        if(!is_null($hits)){
+            $m_hits   = array(927,974,1446,851,1111,1711,1499,1226,285);
+            $m_hit_index = $this->fix_top_number($m_hits,$hits);
+            $num_deci[$m_hit_index] += 1;
+        }
 
         //HR
-        $m_hrs    = array(167,72,94,176,202,183,244,96,12);
-        $m_hr_index = $this->fix_top_number($m_hrs,$hr);
-        $num_deci[$m_hr_index] += 1;
+        if(!is_null($hr)){
+            $m_hrs    = array(167,72,94,176,202,183,244,96,12);
+            $m_hr_index = $this->fix_top_number($m_hrs,$hr);
+            $num_deci[$m_hr_index] += 1;
+        }
 
         //打点
-        $m_rbis   = array(485,331,452,534,749,706,815,451,115);
-        $m_rbi_index = $this->fix_top_number($m_rbis,$rbi);
-        $num_deci[$m_rbi_index] += 1;
+        if(!is_null($rbi)){
+            $m_rbis   = array(485,331,452,534,749,706,815,451,115);
+            $m_rbi_index = $this->fix_top_number($m_rbis,$rbi);
+            $num_deci[$m_rbi_index] += 1;
+        }
 
         //得点
-        $m_scores = array(591,504,749,441,532,880,759,657,105);
-        $m_score_index = $this->fix_top_number($m_scores,$score);
-        $num_deci[$m_score_index] += 1;
+        if(!is_null($score)){
+            $m_scores = array(591,504,749,441,532,880,759,657,105);
+            $m_score_index = $this->fix_top_number($m_scores,$score);
+            $num_deci[$m_score_index] += 1;
+        }
 
         //盗塁
-        $m_steals = array(135,93,167,5,13,147,123,100,6);
-        $m_steal_index = $this->fix_top_number($m_steals,$steal);
-        $num_deci[$m_steal_index] += 1;
+        if(!is_null($steal)){
+            $m_steals = array(135,93,167,5,13,147,123,100,6);
+            $m_steal_index = $this->fix_top_number($m_steals,$steal);
+            $num_deci[$m_steal_index] += 1;
+        }
 
         //出塁率
-        $m_baseps = array(0.401,0.315,0.403,0.381,0.320,0.355,0.325,0.373,0.290);
-        $m_baseps_index = $this->fix_top_number($m_baseps,$basep);
-        $num_deci[$m_baseps_index] += 1;
+        if(!is_null($basep)){
+            $m_baseps = array(0.401,0.315,0.403,0.381,0.320,0.355,0.325,0.373,0.290);
+            $m_baseps_index = $this->fix_top_number($m_baseps,$basep);
+            $num_deci[$m_baseps_index] += 1;
+        }
 
         $m_aves_index = 0;
         $m_aves_value = 100000;
@@ -133,6 +151,8 @@ class ResultController extends Controller
         }
         $res = $max_index + 1;
         $type = $res."番バッタータイプです";
+
+        
 
         return view('results', [
             'type' => $type
